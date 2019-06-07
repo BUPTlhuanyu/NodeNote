d5b295adfbb8f4f239c0671039f83503faf97e6c
lhy <lhuanyutot@163.com>
2019-06-07 23:27:33 +0800

    require

--- nodeChapters/module/a.js
+++ nodeChapters/module/a.js
@@ -5,3 +5,8 @@ console.log('在 a 中，b.done = %j', b.done);
 exports.done = true;
 console.log('a 结束');
 
+console.log("module.require" ,module.require)
+console.log("require" ,require)
+console.log("module." ,module)
+
+console.log("exports" ,exports)
--- nodeChapters/module/index.js
+++ nodeChapters/module/index.js
@@ -3,11 +3,16 @@ const a = require('./a.js');
 const b = require('./b.js');
 console.log('在 main 中，a.done=%j，b.done=%j', a.done, b.done);
 
-
+/**
+ * 被引入的模块将被缓存在这个对象中。从此对象中删除键值对将会导致下一次 require 重新加载被删除的模块。
+ */
 console.log(require.cache)
-
-// console.log("require",require)
-// console.log("module",module)
-// console.log("exports",exports)
-// console.log("__dirname",__dirname)
-// console.log("__filename",__filename)
+/**
+ * 原生require方法会调用mod.require,这个mod.require其实就是调用module.require
+ */
+console.log("module.require",module.require)
+console.log("require",require)
+console.log("module",module)
+console.log("exports",exports)
+console.log("__dirname",__dirname)
+console.log("__filename",__filename)
