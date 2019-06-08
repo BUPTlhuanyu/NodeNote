dfc6726ae89b7fdc2f284daaf0c499d0e7abde74
lhy <lhuanyutot@163.com>
2019-06-08 14:01:38 +0800

    process

---
+++ nodeChapters/process/index.js
@@ -0,0 +1,33 @@
+// process.on('exit', (code) => {
+//     console.log('exit')
+//     setTimeout(() => {
+//       console.log('此处不会运行');
+//     }, 0);
+//   });
+
+//   process.exit()
+
+//   process.on('multipleResolves', (type, promise, reason) => {
+//     console.error(type, promise, reason);
+//     setImmediate(() => process.exit(1));
+//   });
+  
+//   async function main() {
+//     try {
+//       return await new Promise((resolve, reject) => {
+//         resolve('第一次调用');
+//         resolve('吞没解决');
+//         reject(new Error('吞没解决'));
+//       });
+//     } catch {
+//       throw new Error('失败');
+//     }
+//   }
+  
+//   main().then(console.log);
+
+if(process.env.NODE_ENV === 'production'){
+    console.log('生产环境');
+}else{
+    console.log('非生产环境');
+}
