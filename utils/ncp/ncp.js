912b07b5830aadfd90f65c52a22d2d29ed6ca97d
lhy <lhuanyutot@163.com>
2019-06-09 22:51:12 +0800

    ncp.js

--- utils/ncp/ncp.js
+++ utils/ncp/ncp.js
@@ -226,6 +226,8 @@ function ncp (source, dest, options, callback) {
   function isWritable(path, done) {
     fs.lstat(path, function (err) {
       if (err) {
+        // 常见的系统错误
+        // ENOENT (无此文件或目录): 通常是由 fs 操作引起的，表明指定的路径不存在，即给定的路径找不到文件或目录。
         if (err.code === 'ENOENT') return done(true);
         return done(false);
       }
