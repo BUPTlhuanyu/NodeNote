1fb04f0c46e182deab791334274af2b71ac4be40
lhy <lhuanyutot@163.com>
2019-06-15 10:05:17 +0800

    tcp的半关闭状态，socket一端调用end，另一端只能写，读无效；当前端只能读，写无效

--- nodeChapters/TCP-net/client.js
+++ nodeChapters/TCP-net/client.js
@@ -1,15 +1,30 @@
 /**
  * Created by liaohuanyu on 2019/4/21.
+ * socket事件：
+        1.data: 当一端调用write()发送数据时，另一端会触发data事件，事件传递的数据时write()发送的数据。
+        2.end: 当连接中的任意一端发送了FIN数据时，将会触发该事件。 
+        3.connect： 该事件用于客户端，当套接字与服务器连接成功时会被触发。 
+        4.drain： 当任意一端调用write( )发送数据时，当前这端会触发该事件。 
+        5.error： 当异常发生时，会触发该事件。 
+        6.close： 当套接字完全关闭时，触发该事件。 
+        7timeout： 当一定时间后连接不在活跃时，会被触发，通知用户当前该连接已经被闲置。 
+        由于TCP套接字是可读可写的Stream对象，可以利用pipe()方法实现管道操作。 
  */
-var net = require('TCP-net');
+
+
+
+var net = require('net');
 var client = net.connect({port:8124},function(){
     console.log('client connected');
     client.write('world!')
 });
 client.on('data',function(data){
     console.log(data.toString());
+    // end会使得当前socket处于半关闭状态，不能主动写数据到服务端
     client.end();
 });
 client.on('end',function(){
     console.log('client disconnected');
+    // 此时socket处于半关闭状态，只能读不能写
+    client.write('can?')
 })
--- nodeChapters/TCP-net/server.js
+++ nodeChapters/TCP-net/server.js
@@ -1,15 +1,21 @@
 /**
  * Created by liaohuanyu on 2019/4/21.
+ * server事件：
+        1:listening: 在调用sever.listen()绑定端口或者Domain Socket后触发，简洁写法为server.listen（por,listeningListener），通过listen()方法的第二个参数传入。 
+        2.connection: 每个客户端套接字连接到服务器端时触发 
+        3.close： 当服务器关闭时触发，在调用server.close（）后，服务器将停止接受新的套接字连接，但保持当前存在的连接，等待所有连接都断开后，会触发该事件。 
+        4.error： 当服务器发生异常时，会触发该事件。
  */
-var net = require('TCP-net');
+var net = require('net');
 
-var server = net.createServer(function(socket){
+var server = net.createServer({allowHalfOpen:true},function(socket){
     socket.on('data',function(data){
         console.log(data.toString())
         socket.write('你好');
     });
     socket.on('end',function(){
-        console.log('链接断开')
+        console.log('链接断开,allowHalfOpen为true，不能读但是能写')
+        socket.write('allowHalfOpen')
     })
     socket.write('示例')
 })
